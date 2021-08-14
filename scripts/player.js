9c7effb541c1115e679b6e7d6e098a4e11e0b1d1
AlexZ005 <alexz005.info@gmail.com>
2021-08-14 10:39:38 +0300

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -29,7 +29,7 @@ let blockersEnemy = [90, 112, 109]
 let blockersNpc = [119]
 
 function preloadPlayer() {
-    tileset = loadImage("assets/blockPack.png");
+    tileset = loadImage("assets/blockPackpacked.png");
 }
 
 let atk = 0;
